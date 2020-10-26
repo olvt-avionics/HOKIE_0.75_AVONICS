dad846e4a3b380d6d5111e00a0b2aa7b2dec85c5
Aaron V Brown <aaron.victor.brown@gmail.com>
2020-10-26 14:34:02 -0400

    refactored project.

--- projects/rocket_MSP_EXP432E401Y_freertos_gcc/hal_code/i2c_hal.c
+++
@@ -1,9 +0,0 @@
-/*
- * i2c_hal.c
- *
- *  Created on: Mar 6, 2020
- *      Author: toocrazzzzy
- */
-
-/* User Files */
-#include <hal_code/i2c_hal.h>
--- projects/rocket_MSP_EXP432E401Y_freertos_gcc/hal_code/spi_hal.c
+++
@@ -1,9 +0,0 @@
-/*
- * spi_hal.c
- *
- *  Created on: Mar 6, 2020
- *      Author: toocrazzzzy
- */
-
-/* User Files */
-#include "spi_hal.h"
